57207a2e9d12aae1c72ef256ce4053e27d618ac4
YoussefElserwi96 <josephmousa961@gmail.com>
2023-10-08 19:58:46 +0300

    02 task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description: prints alhphabet from a to z
+ * Return: 0 Always (success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++
+	}
+	putchar('\n');
+	return (0);
+}
