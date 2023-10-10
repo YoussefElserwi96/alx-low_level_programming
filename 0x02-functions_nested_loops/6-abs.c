4cc5c7c63c0a9a34dbd25128cb7115ddf20857c1
YoussefElserwi96 <josephmousa961@gmail.com>
2023-10-11 00:38:10 +0300

    task 7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @num : Given number to cehck
+ * Return: Always 0 (Success)
+ */
+int _abs(int num1)
+{
+	if (num1 >= 0)
+	{
+		return (num1);
+	}
+	return (-num1);
+}
