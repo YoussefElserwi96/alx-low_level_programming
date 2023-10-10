caaf74c6476bbdea611c83083f85a6bf31ec1a53
YoussefElserwi96 <josephmousa961@gmail.com>
2023-10-10 23:18:11 +0300

    made the main.h file for tasks. done first task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *main - entry point
+ *Description: printing _putchar utilising prototype
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+		_putchar('\n');
+
+		return (0);
+}
