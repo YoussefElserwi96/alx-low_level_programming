55ef0f7ce058b630653da5f7704b4142d0e504a6
YoussefElserwi96 <josephmousa961@gmail.com>
2023-10-10 23:56:00 +0300

    task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+* main - entry point
+* Description : print the alhpabet
+* Return: Always 0 (success)
+*/
+
+void print_alphabet(void);
+{
+		char A = 'a';
+
+		while (A <= 'z')
+		{
+			_putchar(A);
+			A++;
+		}
+		_putchar('\n');
+}
