cff13e96cbcb21aa98825451b314001b9f0f175e
YoussefElserwi96 <josephmousa961@gmail.com>
2023-10-11 01:38:10 +0300

    task 2 adv  fix010

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: Prints natural numbers below 1024 that are
+ * multiplies of 3 or 5
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 1; num1 < 1024; num1++)
+	{
+		if ((num1 % 3) == 0 || (num1 % 5) == 0)
+		{
+			num2 += num1;
+		}
+	}
+	printf("%d\n", num2);
+	return (0);
+}
