052d59ee400d851005dd9f92b484cd48e5a2370e
YoussefElserwi96 <josephmousa961@gmail.com>
2023-10-17 00:55:50 +0300

    first task fix

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * reset_to_98 - to reset a value of a variable
@@ -6,7 +6,7 @@
  * @n: pointer to the variable to update
  *
 */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
-	*n =98;
+	*n = 98;
 }
