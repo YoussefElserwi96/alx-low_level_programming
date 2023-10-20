7408f9ce36b79a147e796f1f37feb15eba07e75b
YoussefElserwi96 <josephmousa961@gmail.com>
2023-10-20 04:12:35 +0300

    103 02 fixxxx

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -5,43 +5,40 @@
  * @str: the input string to reverse
  * Return: void
  */
+
 void rev_string(char *n)
 {
 	int start = 0;
 	int end = 0;
 	char temp;
 
-	while (*(n + end) != '\0')
+	while (*(n + start) != '\0')
 	{
-		end++;
+		start++;
 	}
-	end--;
+	start--;
 
-	for (start = 0; start < end; start++, end--)
+	for (end = 0; end < start; end++, start--)
 	{
-		temp = *(n + start);
-		*(n + start) = *(n + end);
-		*(n + end) = temp;
+		temp = *(n + end);
+		*(n + end) = *(n + start);
+		*(n + start) = temp;
 	}
 }
 
 /**
  * infinite_add - add 2 numbers together
- * @n1: text representation of the 1st number to add
- * @n2: text representation of the 2nd number to add
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
  * @r: pointer to buffer
  * @size_r: buffer size
  * Return: pointer to calling function
  */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int carry = 0;
-	int i = 0;
-	int j = 0;
-	int digits = 0;
-	int value1 = 0;
-	int value2 = 0;
-	int temp_total = 0;
+	int carrying = 0, i = 0, j = 0, digits = 0;
+	int value1 = 0, value2 = 0, temptotal = 0;
 
 	while (*(n1 + i) != '\0')
 		i++;
@@ -51,7 +48,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	j--;
 	if (j >= size_r || i >= size_r)
 		return (0);
-	while (j >= 0 || i >= 0 || carry == 1)
+	while (j >= 0 || i >= 0 || carrying == 1)
 	{
 		if (i < 0)
 			value1 = 0;
@@ -61,14 +58,14 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			value2 = 0;
 		else
 			value2 = *(n2 + j) - '0';
-		temp_total = value1 + value2 + carry;
-		if (temp_total >= 10)
-			carry = 1;
+		temptotal = value1 + value2 + carrying;
+		if (temptotal >= 10)
+			carrying = 1;
 		else
-			carry = 0;
+			carrying = 0;
 		if (digits >= (size_r - 1))
 			return (0);
-		*(r + digits) = (temp_total % 10) + '0';
+		*(r + digits) = (temptotal % 10) + '0';
 		digits++;
 		j--;
 		i--;
