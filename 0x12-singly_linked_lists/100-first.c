ec5ab7f623408a840767d50776e3339a3fdd398a
YoussefElserwi96 <josephmousa961@gmail.com>
2023-11-17 01:56:49 +0200

    adv tasks testing002

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * bmain - a function that prints
+ * "You're beat! and yet, you must allow,
+ * \nI bore my house upon my back!\n "
+ * before the main function is executed.
+ * Return: no return.
+ */
+
+void __attribute__ ((constructor)) bmain()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
